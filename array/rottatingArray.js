56e4ce34792fe10ff221463ba2ae8bb70af45e18
Fayaz Faizal <fayazblogger@gmail.com>
2024-02-05 15:03:15 +0530

    Rottating Array

---
+++ array/rottatingArray.js
@@ -0,0 +1,21 @@
+var array=[1,2,3,4,5]
+
+var k=2
+
+// // Method 1
+// var le=array.length-1
+
+// for(r=0;r<k;r++){
+//     array.unshift(array[le])
+//     array.pop()
+// }
+// console.log(array);
+
+// // Method 2
+// var array=[1,2,3,4,5]
+// var k=2
+// for(i=0;i<k;i++){
+//     let el=array.pop()
+//     array.unshift(el)
+// }
+// console.log(array);
