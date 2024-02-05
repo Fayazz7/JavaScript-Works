90a35303496cfaaf1c8e4caa34c79a45985a37de
Fayaz Faizal <fayazblogger@gmail.com>
2024-02-05 14:49:06 +0530

    Array and Methods

---
+++ array/array.js
@@ -0,0 +1,19 @@
+var array=[10,20,30,40,50,40]
+
+// there is no length limit
+// duplicates allowed
+// indexing
+
+// console.log(array[1]);
+
+// // printing elements of array using indexing
+// for(i=0;i<array.length;i++){
+//     console.log(array[i]);
+// }
+
+// printing directly 
+// to print index use "in" instead of "of" 
+
+for (let i of array){
+    console.log(i);
+}
---
+++ array/arrayMethod.js
@@ -0,0 +1,16 @@
+// Array Methods
+
+
+var array=[10,20,30,40,50,40]
+
+// array.push(100) //push method is used to add an element at the end of an array
+// console.log(array);
+
+// array.unshift(100) //unshift method is used to add an element at the beggning of an array
+// console.log(array);
+
+// array.pop() //.pop method is used to remove last element of an array
+// console.log(array);
+
+array.shift() //.shift() method is used to remove first element of an array
+console.log(array);
