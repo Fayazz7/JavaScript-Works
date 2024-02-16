da6b91bf8bba5860cc5cf94c8607fd19ab97ebc4
Fayaz Faizal <fayazblogger@gmail.com>
2024-02-17 01:52:45 +0530

    New Note

---
+++ jsRunTimeENV.js
@@ -0,0 +1,21 @@
+
+setTimeout(()=>{
+    console.log("line1");
+},5000)
+
+console.log("line2");
+
+setTimeout(()=>{
+    console.log("line3");
+},2000)
+
+console.log("line4");
+
+setTimeout(()=>{
+    console.log("line5");
+},1000)
+
+/*
+in this program there are two methods log and setTimeout the work flow is first work the log method with js engine and settimeout
+work using wbapi 
+*/
